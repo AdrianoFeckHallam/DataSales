18d3e35a442b4b0c55f42a12739033c29aaf4c06
AdrianoFeckHallam <feckadriano@gmail.com>
2020-12-16 04:28:44 -0300

    Item

---
+++ src/entities/Item.java
@@ -0,0 +1,41 @@
+package entities;
+
+public class Item {
+	private Integer itemID;
+	private Integer itemQuantity;
+	private Double itemPrice;
+	
+	public Item() {
+		
+	}
+
+	public Item(Integer itemID, Integer itemQuantity, Double itemPrice) {
+		this.itemID = itemID;
+		this.itemQuantity = itemQuantity;
+		this.itemPrice = itemPrice;
+	}
+
+	public Integer getItemID() {
+		return itemID;
+	}
+
+	public void setItemID(Integer itemID) {
+		this.itemID = itemID;
+	}
+
+	public Integer getItemQuantity() {
+		return itemQuantity;
+	}
+
+	public void setItemQuantity(Integer itemQuantity) {
+		this.itemQuantity = itemQuantity;
+	}
+
+	public Double getItemPrice() {
+		return itemPrice;
+	}
+
+	public void setItemPrice(Double itemPrice) {
+		this.itemPrice = itemPrice;
+	}
+}
