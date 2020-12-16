56abd75189727465246003b26149454429e09db5
AdrianoFeckHallam <feckadriano@gmail.com>
2020-12-16 04:35:07 -0300

    Sale

---
+++ src/entities/Sale.java
@@ -0,0 +1,43 @@
+package entities;
+
+import java.util.List;
+
+public class Sale {
+	private Integer saleId;
+	private List<Item> items;
+	private String salesmanName;
+	
+	public Sale() {
+		
+	}
+
+	public Sale(Integer saleId, List<Item> items, String salesmanName) {
+		this.saleId = saleId;
+		this.items = items;
+		this.salesmanName = salesmanName;
+	}
+
+	public Integer getSaleId() {
+		return saleId;
+	}
+
+	public void setSaleId(Integer saleId) {
+		this.saleId = saleId;
+	}
+
+	public List<Item> getItems() {
+		return items;
+	}
+
+	public void addItems(Item item) {
+		items.add(item);
+	}
+
+	public String getSalesmanName() {
+		return salesmanName;
+	}
+
+	public void setSalesmanName(String salesmanName) {
+		this.salesmanName = salesmanName;
+	}
+}
