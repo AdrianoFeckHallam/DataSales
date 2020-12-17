83f6806c24ec16a36e4fd89c081cf218d3940b84
AdrianoFeckHallam <feckadriano@gmail.com>
2020-12-17 05:33:02 -0300

    Repair Watcher

--- src/services/Watcher.java
+++ src/services/Watcher.java
@@ -5,8 +5,10 @@ import java.nio.file.FileSystems;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardWatchEventKinds;
+import java.nio.file.WatchEvent;
 import java.nio.file.WatchKey;
 import java.nio.file.WatchService;
+import java.util.Optional;
 
 public class Watcher extends Directory {
 	
@@ -19,7 +21,6 @@ public class Watcher extends Directory {
 			Path directory = Paths.get((pathToDir));
 			directory.register(watcher, StandardWatchEventKinds.ENTRY_CREATE, StandardWatchEventKinds.ENTRY_MODIFY, StandardWatchEventKinds.ENTRY_DELETE);
 			
-			WatchKey key;
 			boolean first = true;
 			while (true) {
 				if (first) {
@@ -27,9 +28,14 @@ public class Watcher extends Directory {
 					Writer.getWriter();
 				}
 
-				key = watcher.take();
-			
-				Writer.getWriter();
+				WatchKey key = watcher.take();
+				Optional<WatchEvent<?>> watchEvent = key.pollEvents().stream().findFirst();
+				if (watchEvent.isPresent()) {
+					if  (watchEvent.get().kind() == StandardWatchEventKinds.OVERFLOW) {
+						continue; 
+					}
+					Writer.getWriter();
+				}
 				
 				boolean valid = key.reset();
 				if (!valid) {
